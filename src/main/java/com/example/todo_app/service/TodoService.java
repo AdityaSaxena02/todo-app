590a6757f5cdc655170fa3f18ddbd2f7b83b635e
AdityaSaxena02 <saxenaaditya1510@gmail.com>
2025-01-26 17:44:02 +0530

    Initial project setup

---
+++ src/main/java/com/example/todo_app/service/TodoService.java
@@ -0,0 +1,31 @@
+package com.example.todo_app.service;
+import com.example.todo_app.model.Todo;
+import com.example.todo_app.repository.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Todo getTodoById(Long id) {
+        return todoRepository.findById(id).orElse(null);
+    }
+
+    public Todo saveTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public void deleteTodoById(Long id) {
+        todoRepository.deleteById(id);
+    }
+
+}
